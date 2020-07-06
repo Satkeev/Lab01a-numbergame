8cc32653a1fe286d7cae97dcb6e8e00da5dfc7e3
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-07-06 15:45:26 -0700

    main method

---
+++ Lab01a-numbergame/Lab01a-numbergame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab01a_numbergame //name of program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Numbers Game");//title
+         
+
+          try
+        {
+          //StartSequence(); //name of method-does not exist in the current context
+        }
+          catch (Exception e) 
+        {
+          Console.WriteLine("Something wrong");
+          Console.WriteLine(e.Message);
+        }
+          finally
+        {
+          Console.WriteLine("Finish");
+        }
+
+          Console.ReadLine();
+        }
+           
+    }
+}
+
